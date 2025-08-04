d72fc15df949ef680b7d2aed086cbc6a32c24fd3
ghktndyd <hwasuyong99@naver.com>
2025-08-04 16:46:30 +0900

    feat(#1): Stock 엔티티에 메서드 추가 및 테스트 코드 작성

--- src/main/java/io/inventory/stock/domain/Stock.java
+++ src/main/java/io/inventory/stock/domain/Stock.java
@@ -32,4 +32,39 @@ public class Stock extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private StockStatus stockStatus;
+
+    public void increaseStock(int quantity) {
+        validatePositive(quantity);
+        this.actualStock += quantity;
+        updateStatus();
+    }
+
+    public void decreaseStock(int quantity) {
+        validatePositive(quantity);
+        if (this.actualStock - quantity < 0) {
+            throw new IllegalArgumentException("재고가 부족합니다.");
+        }
+        this.actualStock -= quantity;
+        updateStatus();
+    }
+
+    private void updateStatus() {
+        if (this.actualStock <= 0) {
+            this.stockStatus = StockStatus.OUT_OF_STOCK;
+            return;
+        }
+
+        if (this.actualStock < this.safetyUnitStock) {
+            this.stockStatus = StockStatus.LOW_STOCK;
+            return;
+        }
+
+        this.stockStatus = StockStatus.IN_STOCK;
+    }
+
+    private void validatePositive(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("수량은 0보다 커야 합니다.");
+        }
+    }
 }
