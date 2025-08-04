f9a7e0a8d43eb0bc82b72ee87792406dc564bf67
ghktndyd <hwasuyong99@naver.com>
2025-08-04 13:42:24 +0900

    feat(#1): BaseEntity 구현

--- src/main/java/io/inventory/InventoryManagementSystemApplication.java
+++ src/main/java/io/inventory/InventoryManagementSystemApplication.java
@@ -2,7 +2,9 @@ package io.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class InventoryManagementSystemApplication {
 
