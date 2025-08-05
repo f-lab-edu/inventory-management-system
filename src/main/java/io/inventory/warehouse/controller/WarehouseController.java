0193e89e73b390ffccaf654f9d6d3f747fac8d51
ghktndyd <hwasuyong99@naver.com>
2025-08-05 15:12:25 +0900

    feat: WarehouseController Create 로직 구현

---
+++ src/main/java/io/inventory/warehouse/controller/WarehouseController.java
@@ -0,0 +1,26 @@
+package io.inventory.warehouse.controller;
+
+import io.inventory.common.response.ApiResponse;
+import io.inventory.warehouse.command.dto.request.WarehouseCreateRequest;
+import io.inventory.warehouse.command.service.WarehouseCommandService;
+import io.inventory.warehouse.query.dto.WarehouseDetailResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/warehouses")
+@RestController
+public class WarehouseController {
+
+    private final WarehouseCommandService warehouseCommandService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<WarehouseDetailResponse>> createWarehouse(@RequestBody WarehouseCreateRequest request) {
+        return ResponseEntity.ok(
+                ApiResponse.success(warehouseCommandService.createWarehouse(request)));
+    }
+}
