661d77ccca8d24f31884591d1698e912524b0d17
ghktndyd <hwasuyong99@naver.com>
2025-08-25 14:00:24 +0900

    feat: 창고 상세 조회 API 구현

--- service/warehouse/src/main/java/inventory/warehouse/controller/WarehouseController.java
+++ service/warehouse/src/main/java/inventory/warehouse/controller/WarehouseController.java
@@ -1,18 +1,21 @@
 package inventory.warehouse.controller;
 
+import inventory.common.api.ApiResponse;
+import inventory.common.exception.CustomException;
 import inventory.warehouse.dto.request.CreateWarehouseRequest;
 import inventory.warehouse.dto.response.WarehouseResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static inventory.common.exception.ExceptionCode.RESOURCE_NOT_FOUND;
 
 @RequestMapping("/api/v1/warehouses")
 @RestController
@@ -22,7 +25,8 @@ public class WarehouseController {
     private static final Map<Long, WarehouseResponse> WAREHOUSE_STORE = new ConcurrentHashMap<>();
 
     @PostMapping
-    public ResponseEntity<WarehouseResponse> createWarehouse(@Valid @RequestBody final CreateWarehouseRequest request) {
+    public ResponseEntity<ApiResponse<WarehouseResponse>> createWarehouse(@Valid @RequestBody final CreateWarehouseRequest request) {
+
         Long id = ID_GENERATOR.getAndIncrement();
 
         WarehouseResponse warehouse = WarehouseResponse.of(
@@ -38,6 +42,14 @@ public class WarehouseController {
         WAREHOUSE_STORE.put(id, warehouse);
 
         return ResponseEntity.created(URI.create("/api/v1/warehouses/" + id))
-                .body(warehouse);
+                .body(ApiResponse.success(HttpStatus.CREATED, warehouse));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ApiResponse<WarehouseResponse>> getWarehouse(@PathVariable final Long id) {
+        WarehouseResponse foundWarehouse = Optional.ofNullable(WAREHOUSE_STORE.get(id))
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_FOUND));
+
+        return ResponseEntity.ok(ApiResponse.success(foundWarehouse));
     }
 }
