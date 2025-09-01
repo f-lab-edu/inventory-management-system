ffb5efd3cd9738f8aa84425e73f8297f794299e2
ghktndyd <hwasuyong99@naver.com>
2025-09-01 16:32:54 +0900

    refactor: WarehouseController 수정
    - createdAt, modifiedAt 반환 값 추가
    - Header에 Location 정보 불필요하여 삭제

---
+++ api/src/main/java/inventory/warehouse/controller/response/WarehouseResponse.java
@@ -0,0 +1,29 @@
+package inventory.warehouse.controller.response;
+
+import java.time.LocalDateTime;
+
+public record WarehouseResponse(
+        Long id,
+        String name,
+        String postcode,
+        String baseAddress,
+        String detailAddress,
+        String managerName,
+        String managerContact,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
+) {
+    public static WarehouseResponse of(
+            final Long id,
+            final String name,
+            final String postcode,
+            final String baseAddress,
+            final String detailAddress,
+            final String managerName,
+            final String managerContact,
+            final LocalDateTime createdAt,
+            final LocalDateTime modifiedAt
+    ) {
+        return new WarehouseResponse(id, name, postcode, baseAddress, detailAddress, managerName, managerContact, createdAt, modifiedAt);
+    }
+}
