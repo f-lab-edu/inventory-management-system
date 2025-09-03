c3edc43817be154534cd47991b612d4e26071e11
ghktndyd <hwasuyong99@naver.com>
2025-09-03 17:12:13 +0900

    feat: Warehouse 생성 로직 구현 및 메모리 저장소 기능 추가
    - WarehouseService에 Warehouse 생성 로직 구현
    
    - WarehouseMemoryRepository에 메모리 기반 저장소 기능 구현
    
    - Warehouse 도메인 클래스 수정:
      - 활성화 상태 필드(active) 추가

--- api/src/main/java/inventory/warehouse/domain/Warehouse.java
+++ api/src/main/java/inventory/warehouse/domain/Warehouse.java
@@ -1,8 +1,11 @@
 package inventory.warehouse.domain;
 
 import lombok.Builder;
+import lombok.Getter;
 
-@Builder
+import java.util.Objects;
+
+@Getter
 public class Warehouse {
 
     private Long warehouseId;
@@ -18,4 +21,30 @@ public class Warehouse {
     private String managerName;
 
     private String managerContact;
+
+    private boolean active = true;
+
+    @Builder
+    public Warehouse(String name, String postcode, String baseAddress, String detailAddress, String managerName, String managerContact) {
+        this.name = name;
+        this.postcode = postcode;
+        this.baseAddress = baseAddress;
+        this.detailAddress = detailAddress;
+        this.managerName = managerName;
+        this.managerContact = managerContact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Warehouse warehouse = (Warehouse) o;
+        return Objects.equals(warehouseId, warehouse.warehouseId) && Objects.equals(name, warehouse.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(warehouseId);
+        result = 31 * result + Objects.hashCode(name);
+        return result;
+    }
 }
--- api/src/main/java/inventory/warehouse/service/WarehouseService.java
+++ api/src/main/java/inventory/warehouse/service/WarehouseService.java
@@ -1,5 +1,7 @@
 package inventory.warehouse.service;
 
+import inventory.warehouse.controller.request.CreateWarehouseRequest;
+import inventory.warehouse.domain.Warehouse;
 import inventory.warehouse.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,18 @@ import org.springframework.stereotype.Service;
 public class WarehouseService {
 
     private final WarehouseRepository warehouseRepository;
+
+    public Warehouse save(CreateWarehouseRequest request) {
+
+        Warehouse warehouse = Warehouse.builder()
+                .name(request.name())
+                .postcode(request.postcode())
+                .baseAddress(request.baseAddress())
+                .detailAddress(request.detailAddress())
+                .managerName(request.managerName())
+                .managerContact(request.managerContact())
+                .build();
+
+        return warehouseRepository.save(warehouse);
+    }
 }
