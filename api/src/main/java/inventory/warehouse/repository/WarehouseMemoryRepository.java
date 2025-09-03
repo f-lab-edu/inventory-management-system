66cd5ce9d2c96b7822fe6c7bae4ab19a2f1cc939
ghktndyd <hwasuyong99@naver.com>
2025-09-03 17:12:13 +0900

    feat: Warehouse 생성 로직 구현 및 메모리 저장소 기능 추가
    - WarehouseService에 Warehouse 생성 로직 구현
    
    - WarehouseMemoryRepository에 메모리 기반 저장소 기능 구현
    
    - Warehouse 도메인 클래스 수정:
      - 활성화 상태 필드(active) 추가
    
    (cherry picked from commit c3edc43817be154534cd47991b612d4e26071e11)

--- api/src/main/java/inventory/warehouse/repository/WarehouseMemoryRepository.java
+++ api/src/main/java/inventory/warehouse/repository/WarehouseMemoryRepository.java
@@ -1,16 +1,31 @@
 package inventory.warehouse.repository;
 
+import inventory.common.exception.CustomException;
+import inventory.common.exception.ExceptionCode;
 import inventory.warehouse.domain.Warehouse;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class WarehouseMemoryRepository implements WarehouseRepository {
+
+    private final Map<Long, Warehouse> store = new ConcurrentHashMap<>();
+    private final AtomicLong autoIncrementId = new AtomicLong();
+
     @Override
     public Warehouse save(Warehouse warehouse) {
-        return null;
+        if (warehouse == null) {
+            throw new CustomException(ExceptionCode.INVALID_INPUT);
+        }
+
+        long key = warehouse.getWarehouseId() == null ? this.autoIncrementId.getAndIncrement() : warehouse.getWarehouseId();
+
+        return store.put(key, warehouse);
     }
 
     @Override
